09f678b6bc6fde1ba2769d039f1e7871633ae9f4
LuoYouRen <s9016010@gmail.com>
2016-06-15 03:44:53 +0800

    anergy bar bug solve(energy zero) Signed-off-by: LuoYouRen <s9016010@gmail.com>

--- snake.js
+++ snake.js
@@ -14,7 +14,6 @@ function Snake(scene){
 			
 			Snake.prototype.move = function(eggs){					
 				if(this.energy == 500){this.die(); return 0;} 
-				if(this.energy<=500 )$(".timeBlock").css("height", this.energy); //能量條下降
 				//判斷尾巴有沒有蛋蛋，有就吃掉
 				for(var i = 0;i<eggs.array.length;i++){
 					if(this.body[this.body.length-1][0] == eggs.array[i].position[0] &&
@@ -124,6 +123,7 @@ function Snake(scene){
 						this.body[0][2] + this.dir[2] == eggs.array[i].position[2]){
 							this.eat.play();
 							this.energy -= 150;
+							$(".timeBlock").css("height", this.energy); //能量條上升
 							if(loser.energy < 0)loser.energy = 0;
 						}
 				}
@@ -132,6 +132,7 @@ function Snake(scene){
 				this.body[0][1]+=this.dir[1];
 				this.body[0][2]+=this.dir[2];
 				this.energy +=5; //能量消耗
+				$(".timeBlock").css("height", this.energy); //能量條下降
 				//set cubes
 				for(var i =0;i<this.body.length;i++){
 					this.cube[i].position = new BABYLON.Vector3(this.body[i][0], this.body[i][1], this.body[i][2]);
